30e3ae66215a3c329d683ebb86eaa4146832646c
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2026-07-26 02:42:29 -0700

    Finish exercises 6-9

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  if (all === 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {(good - bad) / all}</p>
+      <p>Positive {(good / all) * 100} %</p>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
